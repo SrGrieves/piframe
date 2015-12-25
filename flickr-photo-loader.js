6675dd51cecf5535103700b17939d204fbc516b4
Mark Carrier <mark.carrier@srb-es.com>
2015-12-24 23:32:06 -0500

    Moves flickr code into promise and adds express js

--- flickr-photo-loader.js
+++ flickr-photo-loader.js
@@ -3,47 +3,52 @@ var importantStuff = {};
 var _ = require("underscore");
 var https = require('https');
 var fs = require('fs');
+var Flickr = require("flickrapi");
 
 function getRandomPhoto(flickrOptions) {
-  var Flickr = require("flickrapi");
-
-  Flickr.authenticate(flickrOptions, function(error, flickr) {
-    importantStuff.flickr = flickr;
-    flickr.photos.getCounts({ dates:[0,new Date().getTime()]}, function(err, result) {
-      importantStuff.totalNumberOfPhotos = result.photocounts.photocount[0].count;
-      getAPicture();
+  var promise = new Promise(function(resolve, reject) {
+    Flickr.authenticate(flickrOptions, function(error, flickr) {
+      importantStuff.flickr = flickr;
+      flickr.photos.getCounts({ dates:[0,new Date().getTime()]}, function(err, result) {
+        importantStuff.totalNumberOfPhotos = result.photocounts.photocount[0].count;
+        getAPicture(resolve, reject);
+      });
     });
   });
+
+  return promise;
 }
 
-
-function getAPicture() {
-  var randomPhotoNumber = Math.floor(Math.random() * importantStuff.totalNumberOfPhotos);
-  importantStuff.flickr.photos.search({
-    user_id: importantStuff.flickr.options.user_id,
-    authenticated: true,
-    per_page: 1,
-    page: randomPhotoNumber
-  }, handleSearchResult);
+function getAPicture(resolve, reject) {
+    var randomPhotoNumber = Math.floor(Math.random() * importantStuff.totalNumberOfPhotos);
+    importantStuff.flickr.photos.search({
+      user_id: importantStuff.flickr.options.user_id,
+      authenticated: true,
+      per_page: 1,
+      page: randomPhotoNumber
+    }, function(err, searchResult) { handleSearchResult(err, searchResult, resolve, reject); });
 }
 
-function handleSearchResult(err, searchResult) {
+function handleSearchResult(err, searchResult, resolve, reject) {
   console.log("Fetching photo with titled \"" + searchResult.photos.photo[0].title + "\".");
   importantStuff.flickr.photos.getSizes({
     user_id: importantStuff.flickr.options.user_id,
     authenticated: true,
     photo_id: searchResult.photos.photo[0].id
-  }, function(err, sizesResult) { handleGetSizesResult(err,sizesResult,searchResult.photos.photo[0].id); });
+  }, function(err, sizesResult) { handleGetSizesResult(err,sizesResult,searchResult.photos.photo[0], resolve, reject); });
 }
 
-function handleGetSizesResult(err,result, photoId) {
+function handleGetSizesResult(err,result, photoInfo, resolve, reject) {
   var originalPhoto = _.findWhere(result.sizes.size, { label: "Original"});
   if(originalPhoto.media != "photo") {
-    console.log("Landed on a video.  That's bad.");
-    process.exit(1);
+    reject(Error("Landed on a video.  That's bad."));
   } else {
-    var fileStream = fs.createWriteStream(photoId + ".jpg");
-    fileStream.on('finish', function () { console.log("Done."); process.exit(); });
+    var filename = "./photos/" + photoInfo.id + ".jpg";
+    var fileStream = fs.createWriteStream(filename);
+    fileStream.on('finish', function () {
+      photoInfo.file = filename;
+      resolve(photoInfo);
+    });
     var request = https.get(originalPhoto.source, function(response) {
       response.pipe(fileStream);
     });
